e087e6637436a96f6ed73d266e802ed489e2c782
chclever <chcleverf@gmail.com>
2025-06-29 00:30:29 +0300

    Log menu updated.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import customtkinter
 import logger
 import json
+import math
+import time
+
 # Переменные
 custom_font = ("Noto Sans", 22, "bold")
 version = "v1.0.0"
+color_step = 0
 
 # Window cfg
 root = customtkinter.CTk()
@@ -11,6 +15,7 @@ root = customtkinter.CTk()
 root.geometry("800x350")
 root.resizable(False, False)
 root.title(f"[Snake] AutoSender by Fiero | {version}")
+
 # Theme load
 try:
     logger.create_config_if_not_exists()
@@ -32,6 +37,19 @@ try:
         customtkinter.set_appearance_mode("system")
 except Exception as e:
     logger.log(f"Ошибка при загрузке темы/конфига: {e}")
+
+def update_color():
+    global color_step
+    r = int((math.sin(color_step) * 55 + 200))
+    b = int((math.sin(color_step + math.pi) * 55 + 200))
+    g = 50
+    r = max(200, min(255, r))
+    g = max(0, min(100, g))
+    b = max(200, min(255, b))
+    hex_color = f"#{r:02x}{g:02x}{b:02x}"
+    program_name.configure(text_color=hex_color)
+    color_step += 0.05
+    root.after(25, update_color)
 
 def update_theme(switch):
     try:
@@ -61,38 +79,97 @@ content_frame = customtkinter.CTkFrame(root)
 content_frame.pack(side="right", fill="both", expand=True, padx=10, pady=10)
 
 def show_tab(tab_name):
+
+    def clear_logs():
+        textbox.configure(state="normal")
+        textbox.delete("1.0", "end")
+        textbox.configure(state="disabled")    
+    
+
     for widget in content_frame.winfo_children():
         widget.destroy()
     
     if tab_name == "Changelog":
-        label = customtkinter.CTkLabel(content_frame, text="Changelog Content", font=custom_font).pack(pady=20)
+        label = customtkinter.CTkLabel(content_frame, text="Changelog Content", font=custom_font).pack(padx=10, pady=20, anchor = "w")
     elif tab_name == "Panel":
-        label = customtkinter.CTkLabel(content_frame, text="Panel Content", font=custom_font).pack(pady=20)
+        label = customtkinter.CTkLabel(content_frame, text="Panel Content", font=custom_font).pack(padx=10, pady=20, anchor = "w")
     elif tab_name == "Config":
-        label = customtkinter.CTkLabel(content_frame, text="Config Content", font=custom_font).pack(pady=20)
+        label = customtkinter.CTkLabel(content_frame, text="Config Content", font=custom_font).pack(padx=10, pady=20, anchor = "w")
     elif tab_name == "Logs":
-        label = customtkinter.CTkLabel(content_frame, text="Logs Content",font=custom_font).pack(pady=20)
+        label = customtkinter.CTkLabel(content_frame, text="Logs", font=custom_font)
+        label.pack(padx=10, pady=20, anchor = "w")
+        
+        textbox = customtkinter.CTkTextbox(content_frame, state="disabled", wrap="none", scrollbar_button_color="#AAAAAA", scrollbar_button_hover_color="#888888")
+        textbox.pack(fill="both", expand=True, padx=10)
+        
+        clear_btn = customtkinter.CTkButton(content_frame, text="Clear it", font=custom_font, command=clear_logs)
+        clear_btn.pack(fill="both", padx=10, pady=10)
+    
     elif tab_name == "Donate":
         label = customtkinter.CTkLabel(content_frame, text="Donate",font=custom_font).pack(pady=20)
     elif tab_name == "Settings":
-        
-        label = customtkinter.CTkLabel(content_frame, text="Settings",font=custom_font)
-        label.pack(pady=20)
-    
-        switch = customtkinter.CTkSwitch(
-            master=content_frame,
-            text="Dark Theme",
-            command=lambda:update_theme(switch),
-            width=60,
-            height=30,
+        label = customtkinter.CTkLabel(
+            content_frame,
+            text="Settings",
+            font=custom_font,
+            anchor="center"
         )
-        switch.pack(pady=20)
-    
+        label.pack(padx=10, pady=20, anchor = "w")
+        
+        theme_frame = customtkinter.CTkFrame(content_frame, fg_color="transparent")
+        theme_frame.pack(pady=10, padx=20, fill="x")
+        
+        switch = customtkinter.CTkSwitch(
+            master=theme_frame,
+            text="Dark Theme",
+            command=lambda: update_theme(switch),
+            width=40,
+            height=30,
+            font=custom_font
+        )
+        switch.pack(pady=10, padx=20, side="left")
+        
+        delay_frame = customtkinter.CTkFrame(content_frame, fg_color="transparent")
+        delay_frame.pack(pady=10, padx=20, fill="x")
+        
+        delay_label = customtkinter.CTkLabel(
+            delay_frame,
+            text="Delay: 0 ms",
+            font=custom_font
+        )
+        delay_label.pack(pady=20, padx=20, anchor="w")
+        
+        def slider_event(value):
+            if value >= 1:
+                # add_log_message("Slider updated: {value} ")
+                delay_label.configure(text=f"Delay: {int(value)}s or {int(value)//60}m {int(value)%60}s", text_color="white")
+            else:
+                delay_label.configure(text=f"Delay {int(value)}s (too low!)", text_color="red")
+        def save_click():
+            save_btn.configure(text=f"Saved!", text_color="green")
+            
+            
+        delay_entry = customtkinter.CTkSlider(
+            delay_frame,
+            from_=0,
+            to=600,
+            command=slider_event,
+            number_of_steps=360,
+            width=300
+        )
+        delay_entry.pack(pady=10, padx=20, fill="x")
+        delay_entry.set(0)
+        
         if config["SenderSS"]["theme"].lower() == "dark":
             switch.select()
         else:
-            switch.deselect()    
+            switch.deselect()
+        
+        save_btn = customtkinter.CTkButton(content_frame, width=200 ,text="save", font=custom_font, command=save_click)
+        save_btn.pack(pady=5,padx=20, anchor="se")
     
+    
+
 program_name = customtkinter.CTkLabel(sidebar, text="AutoSS " + version, font=custom_font)
 btn_changelog = customtkinter.CTkButton(sidebar, text="Changelog",font=custom_font ,command=lambda: show_tab("Changelog"))
 btn_panel = customtkinter.CTkButton(sidebar, text="Panel", font=custom_font,command=lambda: show_tab("Panel"))
@@ -110,6 +187,7 @@ btn_settings.pack(pady=5, padx=10, fill="x")
 btn_donate.pack(pady=5, padx=10, fill="x")
 
 show_tab("Changelog")
+update_color()
 
 if __name__ == '__main__':
     logger.log("Запускаю приложение...")
